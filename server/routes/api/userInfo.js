73c0ebbaae85a80f6d1dc761789eb57d8b7f4110
Ouvill <git@ouvill.net>
2018-05-12 20:10:43 +0900

    変数変更

--- server/routes/api/userInfo.js
+++ server/routes/api/userInfo.js
@@ -4,7 +4,7 @@ const connection = require('../../utils/pgConnection');
 
 router.post('/', async function (req, res, next) {
     try {
-        userId = req.decoded.userId;
+        userId = req.authorized.userId;
         console.log("userId:" + userId);
         const userRows = await connection.query(' SELECT user_id , user_name, display_name FROM users WHERE user_id=$1 LIMIT 1', [userId]);
         const userInfo = userRows.rows[0];
