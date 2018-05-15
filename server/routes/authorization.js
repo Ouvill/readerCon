2769833ff1627f3fcb9f7248f0b2c9a6815d813d
Ouvill <git@ouvill.net>
2018-05-16 00:51:35 +0900

    認証だけに特化

--- server/routes/authorization.js
+++ server/routes/authorization.js
@@ -1,27 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    const token = req.body.token || req.query.token || req.header['x-access-token'];
-
-    if (!token) {
+    if (!req.auth) {
         res.status(401).json({
             "result": false,
             "message": "you need login."
         });
         return
     }
-    jwt.verify(token, process.env.JWT_KEY, function (err, decoded) {
-        if (err) {
-            console.log(err);
-            return res.status(401).json(
-                {
-                    result: false,
-                    message: 'Invalid jwt token.'
-                }
-            );
-        }
-
-        req.auth = decoded;
-        next();
-    });
+    next();
 }
