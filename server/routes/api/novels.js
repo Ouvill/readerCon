9a710568813e0d65a686ee6e1396d5bb4a016979
Ouvill <git@ouvill.net>
2018-05-16 00:29:24 +0900

    ファイル作成

---
+++ server/routes/api/novels.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/:novelId', function (req, res, next) {
+
+
+});
+
+module.exports = router;
