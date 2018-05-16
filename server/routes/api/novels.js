30b18ebf121286e07c77d692dc2c229075392fd4
Ouvill <git@ouvill.net>
2018-05-16 16:46:54 +0900

    novels のエラーチェックをミドルウェアに

--- server/routes/api/novels.js
+++ server/routes/api/novels.js
@@ -5,18 +5,10 @@ const dbNovel = require('../../utils/db/novels')
 const dbUsers = require('../../utils/db/users')
 const dbChapters = require('../../utils/db/chapters');
 
+let novel = {};
 /* GET users listing. */
-const resNoNovel = (res) => {
-    res.status(404).json({
-        result: false,
-        message: 'no novel',
-        messageJa: '小説が存在しません'
-    });
-}
-
-router.get('/:novelId', async function (req, res, next) {
+const requestNovelIdCheck = (req, res, next) => {
     const novelId = req.params.novelId;
-    const userId = req.auth.userId
     if (!isFinite(novelId)) {
         res.status(400).json({
             result: false,
@@ -25,36 +17,72 @@ router.get('/:novelId', async function (req, res, next) {
         })
         return
     }
+    next();
+}
 
-    const novel = await dbNovel.infoWithContests(novelId);
-    if (!novel) {
-        resNoNovel(res);
-        return
+const existNovel = async (req, res, next) => {
+    try {
+        const novelId = req.params.novelId;
+        novel = await dbNovel.infoWithContests(novelId);
+        if (!novel) {
+            res.status(404).json({
+                result: false,
+                message: 'no novel',
+                messageJa: '小説が存在しません'
+            });
+            return
+        }
+        req.novel = novel
+        next()
+    } catch (err) {
+        console.log(err)
     }
+}
 
-    if (!novel.public && moment(novel.startContestAt).isAfter(moment() && novel.authorId != userId)) {
-        res.status(403).json({
-            result: false,
-            message: 'this novel is not publiced',
-            messageJa: 'この小説はまだ一般公開されていません'
+const isNovelPublic = async (req, res, next) => {
+    try {
+        const novelId = req.params.novelId;
+        const userId = req.auth.userId
+        if (!novel.public && moment(novel.startContestAt).isAfter(moment() && novel.authorId != userId)) {
+            res.status(403).json({
+                result: false,
+                message: 'this novel is not publiced',
+                messageJa: 'この小説はまだ一般公開されていません'
+            })
+            return
+        }
+        next();
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+router.get('/:novelId',
+    requestNovelIdCheck,
+    existNovel,
+    isNovelPublic,
+    async function (req, res, next) {
+        const novelId = req.params.novelId;
+        const userId = req.auth.userId
+
+        if (novel.public || moment(novel.contestPeriod).isBefore(moment()) || novel.authorId == userId) {
+            const author = await dbUsers.publicInfo(novel.authorId);
+            novel.author = author
+        } else {
+            delete novel.authorId
+        }
+        const chapterList = await dbChapters.novelChapters(novelId);
+        novel.chapters = chapterList
+
+        res.json({
+            result: true,
+            message: 'here you are',
+            novel: novel
         })
-        return
-    }
-
-    if (novel.public || moment(novel.contestPeriod).isBefore(moment()) || novel.authorId == userId) {
-        const author = await dbUsers.publicInfo(novel.authorId);
-        novel.author = author
-    } else {
-        delete novel.authorId
-    }
-    const chapterList = await dbChapters.novelChapters(novelId);
-    novel.chapters = chapterList
-
-    res.json({
-        result: true,
-        message: 'here you are',
-        novel: novel
-    })
-});
+    });
 
 module.exports = router;
+
+module.exports.requestNovelIdCheck = requestNovelIdCheck
+module.exports.existNovel = existNovel
+module.exports.isNovelPublic = isNovelPublic
