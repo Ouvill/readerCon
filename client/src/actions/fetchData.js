02edd675f7b9695f2750d54cf05df436b39cade4
Ouvill <git@ouvill.net>
2018-05-17 17:13:13 +0900

    fetchするときのアクション

--- client/src/actions/fetchData.js
+++ client/src/actions/fetchData.js
@@ -1,3 +1,5 @@
+import * as message from './message'
+
 export const REQUEST = 'REQUEST'
 export const RECIEVE = 'RECIEVE'
 export const SHOW_PROGRESS = 'SHOW_PROGRESS'
@@ -11,5 +13,94 @@ export const request = () => {
 export const recieve = () => {
     return {
         type: RECIEVE
+    }
+}
+
+
+const StatusCodeError = class extends Error { }
+StatusCodeError.prototype.name = 'StatusCodeError'
+
+const statusCheck = (response) => {
+    if (!response.ok) {
+        response.json().then((err) => {
+            if (typeof (err.messageJa) !== 'undefined') {
+                throw new StatusCodeError(err.messageJa)
+            }
+            if (typeof (err.message) !== 'undefined') {
+                throw new StatusCodeError(err.message)
+            }
+        })
+    } else {
+        return response.json()
+    }
+}
+
+const fetchGetWithToken = (url, token , data) => {
+    const headers = {
+        'x-access-token': token,
+        mode: 'cors',
+        credentials: 'include'
+    }
+
+
+
+    return fetch(url, {
+        headers,
+        method: 'GET'
+    })
+}
+
+const fetchPostWithToken = (url, token, data) => {
+    const headers = {
+        'x-access-token': token,
+        mode: 'cors',
+        credentials: 'include',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    };
+
+    return fetch(url, {
+        headers,
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
+}
+
+
+const fetchWithToken = (url, method, token, data) => {
+    if (method == 'GET') return fetchGetWithToken(url, token)
+    if (method == 'POST') return fetchPostWithToken(url, token , data)
+}
+
+export const fetchApiAction = (url, method, data, callbackAction, option) => {
+    return (dispatch, getState) => {
+        dispatch(request());
+        fetchWithToken(url, method, data)
+            .then(statusCheck)
+            .then(response => {
+                response.json()
+            })
+            .then(json => {
+                if (json.result) {
+                    dispatch(callbackAction(json, option));
+                } else {
+                    if (typeof (json.messageJa !== 'undefined')) {
+                        dispatch(message.setMessage(json.messageJa))
+                    } else if (typeof (json.messageJa !== 'undefined')) {
+                        dispatch(message.setMessage(json.message))
+                    } else {
+                        dispatch(message.setMessage('サーバーにアクセスできませんでした'))
+                    }
+                }
+            }).catch(err => {
+                console.log(err.stack);
+                if (err instanceof StatusCodeError) {
+                    dispatch(message.setMessage(err.message))
+                } else {
+                    dispatch(message.setMessage('サーバーにアクセスできませんでした'))
+                }
+            }).finally(() => {
+                dispatch(recieve());
+            })
     }
 }
